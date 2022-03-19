2c2d7de1a35884fdb1f4d878479442e9de2f13ff
Admin <Admin@LAPTOP-K9LJBOEG>
2022-03-19 15:44:24 +0530

    Finish Commit

---
+++ tt/src/tt/Main.java
@@ -0,0 +1,15 @@
+package tt;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//int x=5,y=0,z;
+		//z=x/y;
+		int z;
+		for(z=1;z<10;z++)
+		{		
+		   System.out.println(z);
+		}
+	}
+
+}
